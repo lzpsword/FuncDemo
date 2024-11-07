0d34fb632ab81be9d5dd76a4c14a17e3b59f9e15
zpliao <lzpsword@qq.com>
2024-11-07 15:42:41 +0800

    1、add input dialog demo

--- app/src/main/java/com/example/funcdemo/activity/DemoActivity.java
+++ app/src/main/java/com/example/funcdemo/activity/DemoActivity.java
@@ -1,7 +1,12 @@
 package com.example.funcdemo.activity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.funcdemo.R;
@@ -25,36 +30,69 @@ public class DemoActivity extends BaseActivity
 	public void initView() {
 		findViewById(R.id.tm_btn1).setOnClickListener(this);
 		findViewById(R.id.clear).setOnClickListener(this);
+		findViewById(R.id.demo_input_dialog).setOnClickListener(this);
 		mTextView = (TextView) findViewById(R.id.result);
 	}
 
-	@Override
+    @Override
 	public void onClick(View view) {
-		switch (view.getId()) {
-			case R.id.tm_btn1:
-				Log.i("neil", "show Loading 1233213");
-				addLoadingMask("拷贝中，请稍后", false);
-				ExecutorService executor = Executors.newFixedThreadPool(2);
-				executor.execute(new Runnable() {
-					@Override
-					public void run() {
-						AssetUtil.copyFilefromAssets(getAppContext(), "test.zip");
-						try {
-							Thread.sleep(3000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						Log.i(TAG, "shwo ok11");
-						showMsg("ok");
-
+        int id = view.getId();
+        if (id == R.id.tm_btn1) {
+			Log.i("neil", "show Loading 1233213");
+			addLoadingMask("拷贝中，请稍后", false);
+			ExecutorService executor = Executors.newFixedThreadPool(2);
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					AssetUtil.copyFilefromAssets(getAppContext(), "test.zip");
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
 					}
-				});
-				break;
-			case R.id.clear:
-				clearDisplay();
-				break;
-			default:
-				break;
-		}
+					Log.i(TAG, "shwo ok11");
+					showMsg("ok");
+
+				}
+			});
+		} else if (id == R.id.demo_input_dialog) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("登录");
+
+			// 自定义对话框布局
+			View customView = LayoutInflater.from(this).inflate(R.layout.input_dialog_demo, null);
+			builder.setView(customView);
+
+			// 获取用户名和密码输入框
+			EditText usernameInput = customView.findViewById(R.id.input1_text);
+
+			// 添加“登录”按钮
+			builder.setPositiveButton("登录", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					String username = usernameInput.getText().toString();
+
+					// 处理用户输入的用户名和密码，比如进行验证
+					if (username.isEmpty()) {
+						showMsg("input name or password can not be empty");
+					} else {
+						showMsg("username=" + username + ";");
+					}
+				}
+			});
+
+			// 添加“取消”按钮
+			builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+
+			AlertDialog dialog = builder.create();
+			dialog.show();
+        } else if (id == R.id.clear) {
+            clearDisplay();
+        }
 	}
 }
